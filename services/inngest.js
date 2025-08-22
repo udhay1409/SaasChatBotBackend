08305db3812d468c48af9bfd8652934661b0d6e5
your-github-username <manoj@mntfuture.com>
2025-08-22 13:21:40 +0530

    Initial backend commit111

--- services/inngest.js
+++ services/inngest.js
@@ -3,7 +3,9 @@ const { Inngest } = require("inngest");
 // Create an Inngest client
 const inngest = new Inngest({ 
   id: "chatbot-app",
-  name: "ChatBot Application"
+  name: "ChatBot Application",
+    eventKey: process.env.INNGEST_EVENT_KEY,   // 🔑 REQUIRED
+  serveHost: process.env.INNGEST_SERVE_HOST, // Optional
 });
 
 module.exports = { inngest };
